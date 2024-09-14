c8e4f93ddd7487378a7119ecb49be574334beddf
wayneleon1 <rurangwaleon10@gmail.com>
2024-09-14 14:29:05 +0200

    implement Recipe Details Screen

--- src/screens/RecipeDetailsScreen.js
+++ src/screens/RecipeDetailsScreen.js
@@ -1,4 +1,11 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { colors } from "../constant/Contant";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -28,7 +35,7 @@ const RecipeDetailsScreen = ({ navigation, route }) => {
         style={{
           flex: 1,
           backgroundColor: colors.COLOR_LIGHT,
-          marginTop: 200,
+          marginTop: 150,
           borderTopLeftRadius: 56,
           borderTopRightRadius: 56,
           alignItems: "center",
@@ -52,12 +59,110 @@ const RecipeDetailsScreen = ({ navigation, route }) => {
             }}
           />
         </View>
-        <Text style={{ marginTop: 160, fontSize: 28, fontWeight: "bold" }}>
+        <Text style={{ marginTop: 130, fontSize: 28, fontWeight: "bold" }}>
           {item.name}
         </Text>
-        <Text style={{ marginVertical: 16, fontSize: 20 }}>
-          {item.description}
-        </Text>
+        <View style={{ flex: 1 }}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text style={{ marginVertical: 10, fontSize: 20 }}>
+              {item.description}
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "rgba(255,0,0,0.38)",
+                  paddingVertical: 26,
+                  borderRadius: 8,
+                  alignItems: "center",
+                  width: 100,
+                }}
+              >
+                <Text style={{ fontSize: 32 }}>⏰</Text>
+                <Text style={{ fontSize: 18, fontWeight: 400 }}>
+                  {item.time}
+                </Text>
+              </View>
+              <View
+                style={{
+                  backgroundColor: "rgba(135,206,235,0.8)",
+                  paddingVertical: 26,
+                  borderRadius: 8,
+                  alignItems: "center",
+                  width: 100,
+                }}
+              >
+                <Text style={{ fontSize: 32 }}>🍳</Text>
+                <Text style={{ fontSize: 18, fontWeight: 400 }}>
+                  {item.difficulty}
+                </Text>
+              </View>
+              <View
+                style={{
+                  backgroundColor: "rgba(255,165,0,0.48)",
+                  paddingVertical: 26,
+                  borderRadius: 8,
+                  alignItems: "center",
+                  width: 100,
+                }}
+              >
+                <Text style={{ fontSize: 32 }}>🔥</Text>
+                <Text style={{ fontSize: 18, fontWeight: 400 }}>
+                  {item.calories}
+                </Text>
+              </View>
+            </View>
+            <View style={{ alignSelf: "flex-start", marginVertical: 22 }}>
+              <Text
+                style={{ fontSize: 20, fontWeight: "600", marginBottom: 6 }}
+              >
+                Ingredients:
+              </Text>
+              {item.ingredients.map((ingredient, index) => {
+                return (
+                  <View
+                    key={index}
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      marginVertical: 4,
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: "red",
+                        width: 10,
+                        height: 10,
+                        borderRadius: 5,
+                      }}
+                    ></View>
+                    <Text style={{ fontSize: 18, marginLeft: 6 }}>
+                      {ingredient}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
+            <View style={{ alignSelf: "flex-start", marginBottom: 16 }}>
+              <Text
+                style={{ fontSize: 20, fontWeight: "600", marginBottom: 6 }}
+              >
+                Steps:
+              </Text>
+              {item.steps.map((step, index) => {
+                return (
+                  <Text
+                    style={{ fontSize: 18, marginLeft: 6, marginVertical: 6 }}
+                  >{`${index + 1}) ${step}`}</Text>
+                );
+              })}
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </View>
   );
